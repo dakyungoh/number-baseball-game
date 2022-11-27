6e7c066c1cddae9cf7c41c297ab99c4736ea28c7
오다경 <dakyung.work@gmail.com>
2022-11-27 18:10:50 +0900

    feat: 입력값 받아오기 및 유효성 검사

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-const bodyElem = document.querySelector("body");
-console.log(bodyElem);
+// 실행버튼을 누르면 UI 인풋에 입력된 숫자를 받아와서 숫자야구 로직으로 검사합니다.
+function onClickPlay() {
+  const inputStr = getInputStr();
+  if (!isValid(inputStr)) {
+    alert("값이 유효하지 않습니다. 숫자 3개를 입력해주세요.");
+  }
+}
+
+// UI 인풋에 입력된 숫자를 받아온다.
+function getInputStr() {
+  const inputElem = document.querySelector("#answer-input");
+  return inputElem.value;
+}
+
+// 숫자 야구 게임의 입력 값이 적절한지 판단한다.
+function isValid(str) {
+  // check length
+  if (str.length !== 3) {
+    return false;
+  }
+  // check is number
+  for (let i = 0; i < str.length; i++) {
+    if (!Number.isInteger(parseInt(str[i]))) {
+      return false;
+    }
+  }
+  return true;
+}
